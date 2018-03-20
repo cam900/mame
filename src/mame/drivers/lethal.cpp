b3a3708900fc9c51b56d8b615f100fdaa24ff0f8
cam900 <dbtlrchl@naver.com>
2018-03-21 01:47:25 +0900

    lethal.cpp : Minor cleanup

--- src/mame/drivers/lethal.cpp
+++ src/mame/drivers/lethal.cpp
@@ -249,7 +249,6 @@ maybe some sprite placement issues
 #include "emu.h"
 #include "includes/lethal.h"
 
-#include "cpu/m6809/m6809.h"
 #include "cpu/m6809/hd6309.h"
 #include "cpu/z80/z80.h"
 #include "machine/eepromser.h"
