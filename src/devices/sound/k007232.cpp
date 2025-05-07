37b6535bb028f6283d194fcd415d89444e2868ff
RAintN22 <105845701+RAintN22@users.noreply.github.com>
2025-05-07 09:34:55 +0800

    final k007232 vgm log

--- src/devices/sound/k007232.cpp
+++ src/devices/sound/k007232.cpp
@@ -241,8 +241,8 @@ void k007232_device::set_bank(int chan_a_bank, int chan_b_bank)
 	// so libvgm will see the register writes to 0x0E and 0x0F.
 	if (m_vgm_log && m_vgm_log->IsValid())
 	{
-		m_vgm_log->Write(0x00, 0x0E, chan_a_bank);
-		m_vgm_log->Write(0x00, 0x0F, chan_b_bank);
+		m_vgm_log->Write(0x00, 0x14, chan_a_bank);
+		m_vgm_log->Write(0x00, 0x15, chan_b_bank);
 	}
 }
 
